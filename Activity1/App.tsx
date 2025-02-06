65424d4dbd96cfedbd93404f9323f55e954e3e14
RenzCarloZamudio <137681134+RenzCarloZamudio@users.noreply.github.com>
2025-02-06 13:51:07 +0800

    ScrollView

---
+++ Activity1/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={['top']}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+          I wanna lay you down in bed of roses. For tonight I'll sleep on a bed of nails.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 42,
+    padding: 12,
+  },
+});
+
+export default App;
